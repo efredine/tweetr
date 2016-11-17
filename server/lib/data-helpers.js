a6bb4cea6f9415b46b0556cf6c9d6884b01e4c84
Eric Fredine <eric.fredine@gmail.com>
2016-11-17 19:44:49 +0000

    Implement post and retrieve from mongodb.

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,27 +1,35 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
+
+  const tweets = db.collection('tweets');
+
   return {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
+      tweets.insertOne(newTweet, (err, result) => {
+        if(!err && result.insertedCount === 1) {
+          callback(err, result.ops[0]);
+        } else {
+          callback(err, null);
+        }
       });
+      // simulateDelay(() => {
+      //   db.tweets.push(newTweet);
+      //   callback(null, true);
+      // });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      tweets.find().toArray(callback);
+      // simulateDelay(() => {
+      //   const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      //   callback(null, db.tweets.sort(sortNewestFirst));
+      // });
     }
 
   };
-}
+};
